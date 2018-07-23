effce3b929375af7351a571f1dfeef70fe6da8aa
avaschuk <andreivashchuk4@gmail.com>
2018-07-24 01:37:02 +0300

    part 23, deviceFirebaseToken to db while login

--- app/src/main/java/com/andrei/lapitchat/LoginActivity.java
+++ app/src/main/java/com/andrei/lapitchat/LoginActivity.java
@@ -2,21 +2,25 @@ package com.andrei.lapitchat;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private ProgressDialog mLoginProgress;
     private FirebaseAuth mAuth;
+
+    private DatabaseReference mUserDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,8 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         mLoginProgress = new ProgressDialog(this);
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mLoginEmail = findViewById(R.id.login_email);
         mLoginPassword = findViewById(R.id.login_password);
@@ -72,10 +80,26 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             mLoginProgress.dismiss();
-                            Intent main_intent = new Intent(LoginActivity.this, MainActivity.class);
-                            main_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(main_intent);
-                            finish();
+
+                            FirebaseInstanceId.getInstance().getInstanceId()
+                                    .addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                                        @Override
+                                        public void onSuccess(InstanceIdResult instanceIdResult) {
+                                            String deviceToken = instanceIdResult.getToken();
+                                            String current_user_id = mAuth.getCurrentUser().getUid();
+
+                                            mUserDatabase.child(current_user_id).child("device_token").setValue(deviceToken)
+                                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                        @Override
+                                                        public void onSuccess(Void aVoid) {
+                                                            Intent main_intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                            main_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                            startActivity(main_intent);
+                                                            finish();
+                                                        }
+                                                    });
+                                        }
+                                    });
                         } else {
                             mLoginProgress.hide();
                             // If sign in fails, display a message to the user.
