f292190d1cceba4ddc564013bc3ad4553e1148a5
avaschuk <andreivashchuk4@gmail.com>
2018-07-23 22:44:36 +0300

    part 18, continue request feature

--- app/src/main/java/com/andrei/lapitchat/ProfileActivity.java
+++ app/src/main/java/com/andrei/lapitchat/ProfileActivity.java
@@ -74,7 +74,38 @@ public class ProfileActivity extends AppCompatActivity {
                 mProfileName.setText(displayName);
                 mProfileStatus.setText(status);
                 Picasso.get().load(image).placeholder(R.mipmap.default_avatar).into(mProfileImage);
-                mProgressDialog.dismiss();
+
+
+                //-------------------- FRIENDS LIST / REQUEST FEATURE --------------
+
+
+                mFriendReqDatabase.child(mCurrentUser.getUid()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(user_id)) {
+
+                            String req_type = dataSnapshot.child(user_id).child("request_type").getValue().toString();
+                            if (req_type.equals("received")) {
+
+                                mCurrent_state = "req_received";
+                                mProfileSendReqBtn.setText("Accept Friend Request");
+
+                            } else if (req_type.equals("sent")) {
+
+                                mCurrent_state = "req_sent";
+                                mProfileSendReqBtn.setText("Cancel Friend Request");
+
+                            }
+
+                        }
+                        mProgressDialog.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
 
             @Override
@@ -86,6 +117,10 @@ public class ProfileActivity extends AppCompatActivity {
         mProfileSendReqBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                mProfileSendReqBtn.setEnabled(false);
+
+                // ----------------- NOT FRIENDS STATE --------
 
                 if (mCurrent_state.equals("not_friends")) {
 
@@ -100,12 +135,40 @@ public class ProfileActivity extends AppCompatActivity {
                                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                     @Override
                                                     public void onSuccess(Void aVoid) {
+
+                                                        mProfileSendReqBtn.setEnabled(true);
+                                                        mCurrent_state = "req_sent";
+                                                        mProfileSendReqBtn.setText("Cancel Friend Request");
+
                                                         Toast.makeText(ProfileActivity.this, "Request Sent Successfully", Toast.LENGTH_SHORT).show();
                                                     }
                                                 });
                                     } else {
                                         Toast.makeText(ProfileActivity.this, "Failed Sending Request.", Toast.LENGTH_SHORT).show();
                                     }
+                                }
+                            });
+
+                }
+
+                // ----------------- CANCEL REQUEST STATE --------
+
+                if (mCurrent_state.equals("req_sent")) {
+
+                    mFriendReqDatabase.child(mCurrentUser.getUid()).child(user_id).removeValue()
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+
+                                    mFriendReqDatabase.child(user_id).child(mCurrentUser.getUid()).removeValue()
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    mProfileSendReqBtn.setEnabled(true);
+                                                    mCurrent_state = "not_friends";
+                                                    mProfileSendReqBtn.setText("Send Friend Request");
+                                                }
+                                            });
                                 }
                             });
 
